a8212181423dcacb38e6d51f3de54db1dd6019f2
Andrii Loboda <andrii.loboda@kooltra.com>
2020-06-06 17:22:41 -0400

    Clean up.

--- divide-and-conquer/src/main/java/org/aloboda/algo/multiplication/KaratsubaMultiplication.java
+++ divide-and-conquer/src/main/java/org/aloboda/algo/multiplication/KaratsubaMultiplication.java
@@ -8,18 +8,29 @@ import io.vavr.Tuple2;
 import static org.aloboda.algo.multiplication.NumberUtil.addTrailZeros;
 import static org.aloboda.algo.multiplication.NumberUtil.alignToHaveTheSameSize;
 
+/**
+ * Karatsuba Multiplication algorithm for large integer(of any length) numbers.
+ */
 public class KaratsubaMultiplication {
-    private final BasicMathOperations basicMathOperations;
+    private final BasicMathOperations basicMath;
 
     @VisibleForTesting
-    KaratsubaMultiplication(final BasicMathOperations basicMathOperations) {
-        this.basicMathOperations = basicMathOperations;
+    KaratsubaMultiplication(final BasicMathOperations basicMath) {
+        this.basicMath = basicMath;
     }
 
-    KaratsubaMultiplication() {
+    /**
+     * Instantiates this service
+     */
+    public KaratsubaMultiplication() {
         this(new BasicMathOperations());
     }
 
+    /**
+     * @param multiplier   multiplicand to be multiplied by this
+     * @param multiplicand base number
+     * @return product of passed params
+     */
     public CharSequence multiply(final CharSequence multiplier, final CharSequence multiplicand) {
         final Tuple2<CharSequence, CharSequence> multiplierAndMultiplicand = alignToHaveTheSameSize(multiplier, multiplicand);
         return this.multiplyForSameSize(multiplierAndMultiplicand._1, multiplierAndMultiplicand._2);
@@ -37,19 +48,19 @@ public class KaratsubaMultiplication {
         final CharSequence ac = this.multiply(abTuple._1, cdTuple._1);
         final CharSequence bd = this.multiply(abTuple._2, cdTuple._2);
         final CharSequence tuplesProduct = this.multiply(
-                this.basicMathOperations.add(abTuple._1, abTuple._2),
-                this.basicMathOperations.add(cdTuple._1, cdTuple._2)
+                this.basicMath.add(abTuple._1, abTuple._2),
+                this.basicMath.add(cdTuple._1, cdTuple._2)
         );
-        final CharSequence sumAdBc = this.basicMathOperations.subtract(
-                this.basicMathOperations.subtract(tuplesProduct, ac),
+        final CharSequence sumAdBc = this.basicMath.subtract(
+                this.basicMath.subtract(tuplesProduct, ac),
                 bd
         );
         final int length = digitsCount % 2 == 0 ? digitsCount : digitsCount + 1;
         final CharSequence firstPart = addTrailZeros(ac, length);
         final int halfLength = length / 2;
         final CharSequence secondPart = addTrailZeros(sumAdBc, halfLength);
-        return this.basicMathOperations.add(
-                this.basicMathOperations.add(firstPart, secondPart),
+        return this.basicMath.add(
+                this.basicMath.add(firstPart, secondPart),
                 bd
         );
     }
